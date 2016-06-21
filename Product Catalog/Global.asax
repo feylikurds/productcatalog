f2b264540335152abb7da46f35bd4d3455f0a356
dator <dator@DATOR-DATOR>
2016-06-21 23:54:07 +0200

    Add project files.

---
+++ Product Catalog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Product_Catalog.MvcApplication" Language="C#" %>
---
+++ Product Catalog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Product_Catalog.Startup))]
+namespace Product_Catalog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
