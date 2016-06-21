1729468cb1af39b5eb3764cb70b1eca465ecb101
Aryo Pehlewan <aryopehlewan@hotmail.com>
2016-06-22 00:24:29 +0200

    DB

---
+++ Product Catalog/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Product_Catalog.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
